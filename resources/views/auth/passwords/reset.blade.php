8baae2f7e05e105c90bab9caed5d386ee771cff4
GANESH SHETTY <ganesh.ellipsonic@gmail.com>
2017-05-20 00:05:21 +0530

    Finished forgot password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,116 +1,132 @@
-@extends('auth.auth_template')
+    @extends('auth.auth_template')
 
-@section('content')
-<!-- <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+    @section('content')
 
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-                        {{ csrf_field() }}
+                    
+                        <form class="login-form" role="form" method="POST" action="{{ url('/password/reset') }}">
+                            {{ csrf_field() }}
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+                            <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <div class="row">
+              <div class="input-field col s12 center">
+                <img src="\material/img/Logo.ico" alt="" class="responsive-img valign profile-image-login">
+                <p class="center login-form-text">Forgot Password</p>
+              </div>
+            </div>
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} row margin" >
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}">
+                                <div class="input-field col s12">
+                                <i class="mdi-social-person-outline prefix"></i>
+                                <input class="validate" id="email" type="email" name="email" placeholder="Email" value="{{ $email or old('email') }}">
+                                <label for="email" data-error="wrong" data-success="right" class="center-align"></label>
+            
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} row margin">
+                                
+                                <div class="input-field col s12">
+                                    <i class="mdi-action-lock-outline prefix"></i>
+                <input id="password" type="password" class="validate" name="password" placeholder="Password" required>
+                <label for="password"></label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }} row margin">
+                            
+                                <div class="input-field col s12">
+                                    <i class="mdi-action-lock-outline prefix"></i>
+                <input id="password-again" type="password" name="password_confirmation" placeholder="Confirm Password">
+                <label for="password-again"></label>
 
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
+                                    @if ($errors->has('password_confirmation'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-refresh"></i> Reset Password
-                                </button>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <button type="submit" class="btn waves-effect waves-light col s12">
+                                        <i class="fa fa-btn fa-refresh"></i> Reset Password
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="row">
+              <div class="input-field col s6 m6 l6">
+                <p class="margin medium-small"><a href="{{ url('/register')}}">Register Now!</a></p>
+              </div>
+              <div class="input-field col s6 m6 l6">
+              <p class="margin right-align medium-small"><a href="{{ url('/login')}}">Login</a></p>
+              </div>
+            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div> -->
 
-<form class="login-form" method="POST" action="{{ url('/password/email') }}">
-                        {{ csrf_field() }}
-        <div class="row">
-          <div class="input-field col s12 center">
-            <img src="\material/img/Logo.ico" alt="" class="responsive-img valign profile-image-login">
-            <p class="center login-form-text">Forgot Password</p>
-          </div>
-        </div>
-        <div class="row margin">
-          <div class="input-field col s12">
-            <i class="mdi-social-person-outline prefix"></i>
-            <input class="validate" id="email" type="email" name="email" placeholder="Email">
-            <label for="email" data-error="wrong" data-success="right" class="center-align"></label>
-          </div>
-        </div>
-        
-        <div class="row margin">
-          <div class="input-field col s12">
-            <i class="mdi-action-lock-outline prefix"></i>
-            <input id="password" type="password" class="validate" name="password" placeholder="Password" required>
-            <label for="password"></label>
-          </div>
-        </div>
-        <div class="row margin">
-          <div class="input-field col s12">
-            <i class="mdi-action-lock-outline prefix"></i>
-            <input id="password-again" type="password" name="password_confirmation" placeholder="Confirm Password">
-            <label for="password-again"></label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <button class="btn waves-effect waves-light col s12">Recover my Password</button>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s6 m6 l6">
-            <p class="margin medium-small"><a href="{{ url('/register')}}">Register Now!</a></p>
-          </div>
-          <div class="input-field col s6 m6 l6">
-          <p class="margin right-align medium-small"><a href="{{ url('/login')}}">Login</a></p>
-          </div>          
-        </div>
+    <!-- <form class="login-form" role="form" method="POST" action="{{ url('/password/reset') }}" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="token" value="{{ $token }}">
+            <div class="row">
+              <div class="input-field col s12 center">
+                <img src="\material/img/Logo.ico" alt="" class="responsive-img valign profile-image-login">
+                <p class="center login-form-text">Forgot Password</p>
+              </div>
+            </div>
+            <div class="row margin">
+              <div class="input-field col s12">
+                <i class="mdi-social-person-outline prefix"></i>
+                <input class="validate" id="email" type="email" name="email" placeholder="Email">
+                <label for="email" data-error="wrong" data-success="right" class="center-align"></label>
+              </div>
+            </div>
 
-      </form>
-@endsection
+            <div class="row margin">
+              <div class="input-field col s12">
+                <i class="mdi-action-lock-outline prefix"></i>
+                <input id="password" type="password" class="validate" name="password" placeholder="Password" required>
+                <label for="password"></label>
+              </div>
+            </div>
+            <div class="row margin">
+              <div class="input-field col s12">
+                <i class="mdi-action-lock-outline prefix"></i>
+                <input id="password-again" type="password" name="password_confirmation" placeholder="Confirm Password">
+                <label for="password-again"></label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <button type="submit" class="btn waves-effect waves-light col s12" value="Recover my Password">Recover my Password</button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s6 m6 l6">
+                <p class="margin medium-small"><a href="{{ url('/register')}}">Register Now!</a></p>
+              </div>
+              <div class="input-field col s6 m6 l6">
+              <p class="margin right-align medium-small"><a href="{{ url('/login')}}">Login</a></p>
+              </div>
+            </div>
+
+          </form> -->
+    @endsection
